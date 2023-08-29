1e8d3f7303840334c58fb4bdfaafbb99aaa643fe
bartazouhair <bartazouhair@gmail.com>
2023-08-29 02:16:26 -0700

    Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,11 +9,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	size_t lvs = 0;
 
-	if (!tree->right && !tree->left &&)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree)
+	{
+		lvs += (!tree->left && !tree->right) ? 1 : 0;
+		lvs += binary_tree_leaves(tree->left);
+		lvs += binary_tree_leaves(tree->right);
+	}
+	return (lvs);
 }
