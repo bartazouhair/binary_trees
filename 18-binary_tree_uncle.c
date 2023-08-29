c22dafb17d2b7c1b6d8351a156ab1e18c4d24fc5
bartazouhair <bartazouhair@gmail.com>
2023-08-29 02:52:17 -0700

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,37 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - It's a finds the sibling of a node
- * @node: It's a pointer to the node to find the sibling
+ * binary_tree_uncle - It's Finds the uncle of a node
+ *                     in a binary tree.
+ * @node: It's A pointer to the node to find the uncle of.
  *
- * Return: It's pointer to the sibling node
- *         NULL if node is NULL
- *         NULL if the parent is NULL
- *         NULL if the node has no siblings
- */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-	if (!node->parent || !node)
-		return (NULL);
-
-	if (node == node->parent->left)
-		return (node->parent->right);
-	return (node->parent->left);
-}
-
-/**
- * binary_tree_uncle - Its a finds the uncle of a node
- * @node: It's a pointer to the node to find the uncle
- *
- * Return: It's pointer to the uncle node
- *         NULL if node is NULL
- *         NULL if the parent is NULL
- *         NULL if the node has no uncle
+ * Return: It's If node is NULL or has no uncle, NULL.
+ *         Otherwise, a pointer to the uncle node.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node->parent || !node)
+	if (node == NULL ||
+		node->parent == NULL ||
+		node->parent->parent == NULL)
 		return (NULL);
-
-	return (binary_tree_sibling(node->parent));
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
