f8ff85e3e1efaeff19b1076353bbfd91b1204831
bartazouhair <bartazouhair@gmail.com>
2023-08-30 03:02:49 -0700

    Task 40

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -2,9 +2,9 @@
 
 /**
  * treeSize - It's measures the sum of heights of a binary tree
- * @tree: It's pointer to root node of tree to measure the height
+ * @tree: It's pointer to root node of the tree to measure the height
  *
- * Return: Height or 0 if tree is NULL
+ * Return: It's Height or 0 if tree is NULL
  */
 size_t treeSize(const binary_tree_t *tree)
 {
@@ -28,13 +28,13 @@ size_t treeSize(const binary_tree_t *tree)
  * to a sorted array of integers
  *
  * @heap: It's pointer to root node of the heap to convert
- * @size: It's address to store size of the array
+ * @size: It's address to store the size of the array
  *
- * Return: It's pointer to array sorted in descending order
+ * Return: pointer to array sorted in descending order
  **/
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int z, *m = NULL;
+	int j, *m = NULL;
 
 	if (!heap || !size)
 		return (NULL);
@@ -46,8 +46,8 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	if (!m)
 		return (NULL);
 
-	for (z = 0; heap; z++)
-		a[z] = heap_extract(&heap);
+	for (j = 0; heap; j++)
+		a[j] = heap_extract(&heap);
 
 	return (m);
 }
