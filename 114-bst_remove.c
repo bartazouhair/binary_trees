cef5aa454ca2b9fdf996b70cebbe32c93a534dd9
bartazouhair <bartazouhair@gmail.com>
2023-08-31 02:27:17 -0700

    UPD

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,128 +1,55 @@
 #include "binary_trees.h"
 
 /**
- * findNode - It's Finds a node given value in a binary search tree.
- * @root: It's The root of the binary search tree.
- * @value: It's The value of the node.
- * Return: It's A pointer to the found node, otherwise NULL.
+ * bstMinVal - It's finds smallest node from Binary Search Tree
+ * @rt: It's a pointer to the root node of the tree
+ * Return: It's a pointer to the smallest node
  */
-bst_t *findNode(bst_t *root, int value)
+bst_t *bstMinVal(bst_t *rt)
 {
-	bst_t *nde = NULL;
+	bst_t *mn = rt;
 
-	if (root != NULL)
-	{
-		if (root->left != NULL)
-			nde = root->left->parent;
-		if ((nde == NULL) && (root->right != NULL))
-			nde = root->right->parent;
-		while (nde != NULL)
-		{
-			if (nde->n < value)
-				nde = nde->right;
-			else if (nde->n > value)
-				nde = nde->left;
-			else
-				break;
-		}
-	}
-	return (nde);
+	while (mn->left)
+		mn = mn->left;
+
+	return (mn);
 }
 
 /**
- * replaceSuccessor - It's Replaces a node with its inorder \
- * successor in a binary search tree.
- * @node: It's A pointer to the node in the binary search tree.
- *
- * Return: It's A pointer to the node's inorder successor node.
- */
-bst_t *replaceSuccessor(bst_t **node)
-{
-	bst_t *nwNode;
-
-	nwNod = (*node)->right;
-	while ((nwNod != NULL) && (nwNod->left != NULL))
-		nwNod = nwNod->left;
-	if ((nwNod != NULL) && (nwNod == (*node)->right))
-	{
-		nwNod->parent = (*node)->parent;
-		nwNod->left = (*node)->left;
-		if ((*node)->left != NULL)
-			(*node)->left->parent = nwNod;
-		if ((*node)->parent != NULL)
-		{
-			if ((*node)->parent->left == *node)
-				(*node)->parent->left = nwNod;
-			if ((*node)->parent->right == *node)
-				(*node)->parent->right = nwNod;
-		}
-	}
-	else if (nwNod != NULL)
-	{
-		nwNod->parent->left = nwNod->right;
-		if (nwNod->right != NULL)
-			nwNod->right->parent = nwNod->parent;
-		nwNod->parent = (*node)->parent;
-		nwNod->left = (*node)->left;
-		nwNod->right = (*node)->right;
-		(*node)->left->parent = nwNod;
-		(*node)->right->parent = nwNod;
-		if ((*node)->parent != NULL)
-		{
-			if ((*node)->parent->left == *node)
-				(*node)->parent->left = nwNod;
-			if ((*node)->parent->right == *node)
-				(*node)->parent->right = nwNod;
-		}
-	}
-	return (nwNod);
-}
-
-/**
- * bst_remove - It's Removes node given value in a binary search tree.
- * @root: It's The binary search tree.
- * @value: It's The value of the node.
- *
- * Return: It's A pointer to the tree's root node, otherwise NULL.
+ * bst_remove - It's removes a node from a Binary Search Tree
+ * @root: It's a pointer to root node of the tree where you will remove a node
+ * @value: It's the value to remove in the tree
+ * Return: It"s a pointer to new root node of the tree after removal
+ *         NULL on failure
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *nde = NULL, *nwRoot = root, *nwNod = NULL;
+	bst_t *tp = NULL;
 
-	if (nwRoot != NULL)
+	if (!root)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
 	{
-		nde = findNode(root, value);
-		if ((nde != NULL) && (nde->n == value))
+		if (!root->left)
 		{
-			if ((nde->left == NULL) && (nde->right == NULL))
-			{
-				if (nde->parent != NULL)
-				{
-					if (nde->parent->left == nde)
-						nde->parent->left = NULL;
-					if (nde->parent->right == nde)
-						nde->parent->right = NULL;
-				}
-			}
-			else if ((nde->left != NULL) ^ (nde->right != NULL))
-			{
-				nwNod = (nde->left != NULL ? nde->left : nde->right);
-				if (nde->parent != NULL)
-				{
-					if (nde->parent->left == nde)
-						nde->parent->left = nwNod;
-					else if (nde->parent->right == nde)
-						nde->parent->right = nwNod;
-				}
-				nwNod->parent = nde->parent;
-			}
-			else
-			{
-				nwNod = replaceSuccessor(&nde);
-			}
-			nwRoot = (nde->parent == NULL ? nwNod : root);
-			free(nde);
+			tp = root->right;
+			free(root);
+			return (tp);
 		}
+		else if (!root->right)
+		{
+			tp = root->left;
+			free(root);
+			return (tp);
+		}
+		tp = bstMinVal(root->right);
+		root->n = tp->n;
+		root->right = bst_remove(root->right, tp->n);
 	}
-	return (nwRoot);
+	return (root);
 }
